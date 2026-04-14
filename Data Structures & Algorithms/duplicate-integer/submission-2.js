0d17afd96f67943454d83b272daf38ec5d76b4ac
Praise O <oshilimpraisek@gmail.com>
2026-04-14 07:21:34 +0100

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const seenNums = new Set([]);
+
+        for (let i=0; i < nums.length; i++) {
+            if ( seenNums.has(nums[i]) ) {
+                return true;
+            }
+            seenNums.add(nums[i])
+        }
+        return false;
+
+
+        // const uniqueArr = new Set(nums)
+        // if (nums.length !== uniqueArr.size ){
+        //     return true
+        // }
+        // return false;
+    }
+}
